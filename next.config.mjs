e77441678b8f92e8f0441a614c6105b35789cce6
Mito <aaditya.uwwu@gmail.com>
2024-07-17 13:41:36 +0530

    fixed again the eslint rule error in vercel deployement

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config){
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: [{loader: '@svgr/webpack', options: {icon:true}}],
-        })
-        return config;
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images:{
-        remotePatterns:[
-            {
-                hostname:"img.clerk.com",
-            },
-        ],
-    },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
